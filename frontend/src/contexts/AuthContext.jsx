04f6efb45567d2c798ec49e6bc345049dc3715b7
Zhuolin <futommybms@gmail.com>
2025-11-24 01:44:43 -0500

    upload

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext(null);
 
 // TODO: get the BACKEND_URL.
 const VITE_BACKEND_URL =
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+  process.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 /*
  * This provider should export a `user` context state that is
